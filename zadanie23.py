b1c65fa793c5a2eeb846354269077c77ba411f92
Michal <michal.hanebach@gmail.com>
2017-10-30 10:02:31 +0100

    add and implement rot13 cipher module

---
+++ zadanie23.py
@@ -0,0 +1,19 @@
+def rot13(in_string):
+    decoded = ""
+    for char in in_string:
+        if "a" <= char <= "z":
+            decoded += chr(ord("a") + (ord(char) - ord("a") + 13) % 26)
+        elif "A" <= char <= "Z":
+            decoded += chr(ord("A") + (ord(char) - ord("A") + 13) % 26)
+        else:
+            decoded += char
+    return decoded
+
+
+def main():
+    string = "Pnrfne pvcure? V zhpu cersre Pnrfne fnynq!"
+    print(rot13(rot13(string)))
+    print(rot13(string))
+
+
+if __name__ == "__main__": main()
