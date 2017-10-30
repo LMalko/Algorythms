1fd46e4ede48d51e89e5442ca6fd51113b1cc7de
Leanka <comamydzisiajnasniadanie@gmail.com>
2017-10-30 21:22:43 +0100

    Refactor: Change mode from r&w to r+, add seek(0) call overwrite file

--- zadanie_IO_7.py
+++ zadanie_IO_7.py
@@ -5,13 +5,14 @@
 
 
 def number_lines_in_file(filename):
-    with open(filename) as f:
+    with open(filename, 'r+') as f:
         file_content = f.readlines()
 
-    for index in range(len(file_content)):
-        file_content[index] = "{} {}".format(index+1, file_content[index])
+        for index in range(len(file_content)):
+            file_content[index] = "{} {}".format(index+1, file_content[index])
 
-    with open(filename, 'w') as f:
+        f.seek(0)
         f.writelines(file_content)
-        
+
+
 number_lines_in_file('test_file_for_exe_IO_7.txt')
