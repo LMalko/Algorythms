9d9b0b6decc948434198384e27828b3fc9406c71
Leanka <comamydzisiajnasniadanie@gmail.com>
2017-10-30 20:49:46 +0100

    Add numering lines from file

--- zadanie_IO_7.py
+++ zadanie_IO_7.py
@@ -8,5 +8,7 @@ def number_lines_in_file(filename):
     with open(filename) as f:
         file_content = f.readlines()
 
+    for index in range(len(file_content)):
+        file_content[index] = "{} {}".format(index+1, file_content[index])
 
 number_lines_in_file('test_file_for_exe_IO_7.txt')
