77b9a9cc587a84d958738cc26955e97570b0319e
jarqprog <jarqprog@gmail.com>
2017-10-30 11:14:20 +0100

    add zadanie 25

--- zad25.py
+++ zad25.py
@@ -1,21 +1,18 @@
 """Alghorithmic warm-ups: zadanie25."""
 
 
-# Your task in this exercise is to define a function make_3sg_form()
-# which given a verb in infinitive form returns its third person singular form.
-# Test your function with words like try, brush, run and fix.
-# Note however that the rules must be regarded as heuristic, in the sense that you must
-# not expect them to work for all cases.
-# Tip: Check out the string method endswith().
-
 def make_3sg_form(verb):
-    irregulars_suffixes = ['SS', 'X', 'CH', 'SH' or 'O']
-
-    _verb_suffix = ''
-    _verb_3sg_form = ''
-    # SS, X, CH, SH or the letter O
+    _founded_suffix = False
+    irregulars_suffixes = ['ss', 'x', 'ch', 'sh',  'o', 'y']
+    for suffix in irregulars_suffixes:
+        if verb.endswith(suffix):
+            _founded_suffix = suffix
+            break
+    if _founded_suffix:
+        if suffix in ['ss', 'x', 'ch', 'sh',  'o']:
+            return verb + 'es'
+        elif suffix == 'y':
+            _output_verb = verb[0:-1] + 'ies'
+            return _output_verb
     else:
         return verb + 's'
-
-
-print(make_3sg_form('make'))
