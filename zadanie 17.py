4fa6ccb593f9b8a646a30faba1c47d2b5aebc644
LMalko <lmalko2601@gmail.com>
2017-10-30 11:07:57 +0100

    import re

--- zadanie 17.py
+++ zadanie 17.py
@@ -1,13 +1,6 @@
 import re
 
 
-# Write a version of a palindrome recognizer that also accepts phrase palindromes such as 
-# "Go hang a salami I'm a lasagna hog.", "Was it a rat I saw?", "Step on no pets", 
-# "Sit on a potato pan, Otis", "Lisa Bonet ate no basil", "Satan, oscillate my metallic sonatas", 
-# "I roamed under it as a tired nude Maori", "Rise to vote sir", or the exclamation 
-# "Dammit, I'm mad!". Note that punctuation, capitalization, and spacing are usually ignored.
-
-
 def palindrome_recognizer(string):
     regex = re.compile(r"[^A-Za-z]")
     string = regex.sub('', string).lower()
@@ -21,4 +14,6 @@ def palindrome_recognizer(string):
             return False
     return True
 
+
+print(palindrome_recognizer(	"Go hang a salami I'm a lasagna hog."))
 # Lukasz
