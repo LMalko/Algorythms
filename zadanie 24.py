c1b55e5530690c9644f63ec75a4ffffd4adf532a
LMalko <lmalko2601@gmail.com>
2017-10-30 11:03:30 +0100

    FUNCTION DEFINED

---
+++ zadanie 24.py
@@ -0,0 +1,16 @@
+import re
+
+
+# Define a simple "spelling correction" function correct() that takes a string and sees to it that 1) two or more
+# occurrences of the space character is compressed into one, and 2) inserts an extra space after a period if the
+# period is directly followed by a letter. E.g. correct("This   is  very funny  and    cool.Indeed!") should
+# return "This is very funny and cool. Indeed!" Tip: Use regular expressions!
+
+def spelling_correction(string):
+
+    regex = re.compile(r" {2,}")
+    string = regex.sub(" ", string)
+
+    regex = re.compile(r"(\.)([A-Z])")
+    string = regex.sub(r"\1 \2", string)
+    return string
